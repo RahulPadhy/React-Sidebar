c0e2e7aed135220cdad1fb710041cf1b1df13d37
Rahul Padhy <rahulpadhyrkp@gmail.com>
2022-07-01 07:18:47 +0530

    Added Contents

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Sidebar from "./components/Sidebar";
+import ProductList from "./pages/ProductList";
+import Product from "./pages/Product";
+import About from "./pages/About";
+import Comment from "./pages/Comment";
+import Analytics from "./pages/Analytics";
+import Dashboard from "./pages/Dashboard";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Sidebar>
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/analytics" element={<Analytics />} />
+          <Route path="/comment" element={<Comment />} />
+          <Route path="/product" element={<Product />} />
+          <Route path="/productList" element={<ProductList />} />
+        </Routes>
+      </Sidebar>
+    </BrowserRouter>
   );
 }
-
-export default App;
---
+++ src/pages/ProductList.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function ProductList() {
+  return (
+    <div>
+        <h1>
+            ProductList Page
+        </h1>
+    </div>
+  )
+}
